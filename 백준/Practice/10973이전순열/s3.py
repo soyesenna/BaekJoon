cd3244a62f20350751bf04e7e23bde268e3d4119
kimjooyoung <kjy915875@gmail.com>
2023-05-16 00:32:43 +0900

    spare test

--- 백준/Practice/10973이전순열/s3.py
+++ 백준/Practice/10973이전순열/s3.py
@@ -1,9 +1,20 @@
 import sys
-
-def do_permutation(li, target):
-    
+import itertools
 
 n = int(sys.stdin.readline())
 
-li = list(map(int, sys.stdin.readline().split()))
+li = tuple(map(int, sys.stdin.readline().split()))
+
+nums = [i for i in range(1, n + 1)]
+
+per1 = list(itertools.permutations(nums, n - 1))
+#per2 = list(itertools.permutations(nums, n - 1))
+
+print(li[1:])
+print(per1[0])
+if li[1:] == per1[0]:
+    print(str(li[0] - 1) + ''.join(per1[-1]))
+else:
+    print(str(li[0]) + ''.join(per1[per1.index(li[1:]) - 1]))
+
 
