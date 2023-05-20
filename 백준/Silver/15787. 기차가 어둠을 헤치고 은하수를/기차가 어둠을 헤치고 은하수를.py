0bb9dfaee0beec5da23584f11fe328f7f11a7688
soyesenna <85686722+soyesenna@users.noreply.github.com>
2023-05-20 20:28:49 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 404 ms, Memory: 69684 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
@@ -1,25 +1,53 @@
-n, m = map(int, input().split())
-train = [[0]*20 for _ in range(n)]
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+if n == 1:
+    print(1)
+    sys.exit()
+
+def first_order(train, x):
+    if train[x - 1]:
+        return train
+    train[x - 1] = True
+    return train
+
+def second_order(train, x):
+    if not train[x - 1]:
+        return train
+    train[x - 1] = False
+    return train
+
+def third_order(train: list):
+    train.pop()
+    train.insert(0, False)
+    return train
+
+def fourth_order(train: list):
+    del train[0]
+    train.append(False)
+    return train
+
+orders = []
 for _ in range(m):
-    order = list(map(int, input().split()))
-    if order[0]==1:    #승차
-        train[order[1]-1][order[2]-1] = 1
+    orders.append(list(map(int, sys.stdin.readline().split())))
 
-    elif order[0]==2:  #하차
-        train[order[1]-1][order[2]-1] = 0
+trains = [[False for _ in range(20)] for _ in range(n)]
 
-    elif order[0]==3:  #한 칸씩 뒤로
-        for i in range(19,0,-1):
-            train[order[1]-1][i] = train[order[1]-1][i-1]
-        train[order[1]-1][0] = 0
 
-    else:           #한 칸씩 앞으로
-        for i in range(19):
-            train[order[1]-1][i] = train[order[1]-1][i+1]
-        train[order[1]-1][19] = 0
+
+for order in orders:
+    if order[0] == 1:
+        trains[order[1] - 1] = first_order(trains[order[1] - 1], order[2])
+    elif order[0] == 2:
+        trains[order[1] - 1] = second_order(trains[order[1] - 1], order[2])
+    elif order[0] == 3:
+        trains[order[1] - 1] = third_order(trains[order[1] - 1])
+    elif order[0] == 4:
+        trains[order[1] - 1] = fourth_order(trains[order[1] - 1])
+
 
 result = []
-for t in train:
+for t in trains:
     if t not in result:
         result.append(t)
 print(len(result))
