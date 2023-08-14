4e185a4f4c41ce080fc2b4c7c12671c9ec4de092
soyesenna <85686722+soyesenna@users.noreply.github.com>
2023-08-14 16:12:41 +0900

    [Silver V] Title: 귀걸이, Time: 164 ms, Memory: 15020 KB -BaekjoonHub

---
+++ 백준/Silver/1380. 귀걸이/귀걸이.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int idx = 1;
+		while(true) {
+			int n = Integer.parseInt(br.readLine());
+			if (n == 0) break;
+			List<String> names = new ArrayList<>();
+			boolean[] c = new boolean[n];
+			for(int i = 0; i < n; i++) {
+				names.add(br.readLine());
+				c[i] = true;
+			}
+			for (int i = 0; i < 2 * n - 1; i++) {
+				String[] line = br.readLine().split(" ");
+				int k = Integer.parseInt(line[0]) - 1;
+				c[k] = !c[k];
+			}
+			int k = IntStream.range(0, n).filter(i -> !c[i]).findFirst().orElse(-1);
+			System.out.println(idx++ + " " + names.get(k));
+		}
+	}
+}
