ec8cbbbbe670e8b0b4cf10394b3767600eeed7fb
soyesenna <85686722+soyesenna@users.noreply.github.com>
2023-09-12 11:43:14 +0900

    [Gold V] Title: AC, Time: 864 ms, Memory: 110108 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.java
@@ -0,0 +1,78 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		List<StringBuffer> result = new ArrayList<>();
+		
+		for (int i = 0; i < T; i++) {
+			String p = br.readLine();
+			int n = Integer.parseInt(br.readLine());
+			
+			//true : 정방향, false : 역방
+			boolean flag = true;
+			
+			Deque<Integer> nowDeq = new ArrayDeque<>();
+			
+			String now = br.readLine();
+			String[] numStr = now.substring(1, now.length() - 1).split(",");
+			
+			try {
+				for (String s : numStr) nowDeq.add(Integer.parseInt(s));
+			}catch(Exception e) {
+				
+			}
+			
+			
+			if (n == 0) {
+				if (p.contains("D")) result.add(new StringBuffer("error"));
+				else result.add(new StringBuffer("[]"));
+				continue;
+			}
+			
+			boolean error = false;
+			for (char order : p.toCharArray()) {
+				if (order == 'R') {
+					flag = !flag;
+				}
+				else {
+					if (!delete(nowDeq, flag)) {
+						result.add(new StringBuffer("error"));
+						error = true;
+						break;
+					}
+				}
+			}
+			if (!error)result.add(printDeq(nowDeq, flag));
+		}
+		
+		for (StringBuffer s : result) System.out.println(s);
+	}
+	
+	public static StringBuffer printDeq(Deque<Integer> deq, boolean flag) {
+		StringBuffer result = new StringBuffer("[");
+		if (deq.size() == 0) result.append("]");
+		else {
+			if (flag) while (deq.size() > 0) result.append(deq.pollFirst() + ",");
+			else while (deq.size() > 0) result.append(deq.pollLast() + ",");
+			
+			result.deleteCharAt(result.length() - 1);
+			result.append("]");
+		}
+		return result;
+	}
+	
+	public static boolean delete(Deque<Integer> deq, boolean flag) {
+		if (deq.size() == 0) return false;
+		if (flag) deq.remove();
+		else deq.removeLast();
+		return true;
+	}
+
+}
