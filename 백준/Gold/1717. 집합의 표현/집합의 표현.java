905415d3dc07f96242c7089650f1f3e9713bfd2e
soyesenna <85686722+soyesenna@users.noreply.github.com>
2023-11-08 21:02:00 +0900

    [Gold V] Title: 집합의 표현, Time: 492 ms, Memory: 53896 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,53 @@
+
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    public static int N, M;
+    public static int[] arr;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N + 1];
+
+        for (int i = 0; i <= N; i++) arr[i] = i;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int order = Integer.parseInt(st.nextToken());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+
+            if (order == 0) union(n1, n2);
+            else {
+                if (findRoot(n1) == findRoot(n2)) sb.append("YES");
+                else sb.append("NO");
+                sb.append("\n");
+            }
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(sb.toString());
+
+        bw.close();
+        br.close();
+    }
+
+    public static void union(int n1, int n2) {
+        n1 = findRoot(n1);
+        n2 = findRoot(n2);
+        if (n1 != n2) {
+            arr[n1] = n2;
+        }
+    }
+
+    public static int findRoot(int n) {
+        if (n == arr[n]) return n;
+        return arr[n] = findRoot(arr[n]);
+    }
+}
